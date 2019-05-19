6cb697ada178d12da8b99759f89ebb50d9370f8e
elsaversailles <50725388+elsaversailles@users.noreply.github.com>
2019-05-19 19:58:52 +0800

    Version 2 of Random Porn Query Generator
    Changelog:
    1) Added more categories in the result
    2) More list added to generate random from...

---
+++ Random Porn query generator.py
@@ -0,0 +1,80 @@
+import random
+
+
+print ("WELCOME TO RANDOM PORN QUERY GENERATOR!")
+print ("---------------")
+print("Hello, Pornbuff!")
+print("--------------")
+print ("--------------")
+print ("Written by Elsa Versailles, GNU General Public License v3.0")
+print("|            |")
+print("|            |")
+print("Press ENTER to Generate Query")
+input("")
+
+#p1 Porncategory 1 dataset
+p1 = ["Popular With Women", "Verified", "Amateurs", "Verified Models", "Virtual Reality", "60FPS", "Amateur", "Anal", "Arab", "Asian", "Babe", "Babysitter", "BBW", "Behind The Scenes", "Big Ass", "Big Dick", "Big Tits", "Bisexual Male", "Blonde", "Blowjob", "Bondage", "Brazilian", "British", "Brunette", "Bukkake", "Cartoon", "Casting","Celebrity", "Closed Captions", "College", "Compilation", "Cosplay", "Creampie", "Cuckold", "Cumshot", "Czech", "Described Video", "Double Penetration", "Ebony", "Euro", "Exclusive", "Feet", "Female Orgasm", "Fetish", "Fingering", "Fisting", "French", "Funny", "Gangbang", "Gay", "German", "Handjob", "Hardcore" "HD Porn", "Hentai", "Indian", "Interactive", "Interracial", "Italian", "Japanese", "Korean", "Latina", "Lesbian", "Massage", "Masturbation", "Mature," "MILF", "Muscular Men", "Music", "Old/Young", "Orgy", "Parody", "Party", "Pissing", "Pornstar", "POV", "Public", "Pussy Licking", "Reality", "Red Head", "Role Play", "Romantic", "Rough Sex", "School", "Scissoring", "SFW", "Small Tits", "Smoking", "Solo Female", "Solo Male", "Squirt", "Step Fantasy", "Strap On", "Striptease", "Tattooed Women", "Teen", "Threesome", "Toys", "Trans Male", "Trans With Girl", "Trans With Guy", "Transgender","Verified Couples", "Vintage", "Webcam",]
+
+#p2 list was country based on 2018 pornhub insights
+
+p2 = ["Philippines/Pinay", "South Africa", "United States", "Ukraine/Ukranian", "Canada/Canadian", "United Kingdom", "France/French", "Argentina", "Netherlands", "Australia/Australian", "Poland/Polish", "Germany", "Sweden/Swedish", "Mexico/Mexican", "Italy/Italian","Spain/Spanish", "Brazil/Brazillian", "Japan/Japanesse", "India/Indian", "Russia/Russian", "Czech", "Algeria", "Morocco", "Sri Lanka/Sri Lankan", "Fiji", "Latvia", "Ethiopia", "Hongkong", "China/Chinese", "Korea/Korean", "Taiwan/Taiwanesse", "Greece/Greek", "Iceland", "Austria", "Cyprus","Belarus", "Columbia", "Bulgaria", "Finland",]
+
+
+#To be updated
+
+#p3 Characteristics
+
+p3 = ["Big tits", "Petite", "Red head", "Black", "Tall", "Brunette", "White", "Small tits",]
+
+#p4 profession
+
+p4 = ["Cop", "Doctor", "Teacher", "Driver", "Instructor", "Student", "Mother", "Father", "Writer", "Pornstar", "Actor", "Actress",]
+
+#p5 Fetish 
+
+p5 = ["Foot Fetish", "Glass Fetish", "Hair Fetish", "Pee Fetish", "Latex", "Navel", "Shoe Fetishism", "Underwear Fetishism", "Erotic Asphyxiation", "Stimming", "Looning", "Dendrophillia", "Clothing Fetishism", "Leather", "Adult Breastfeeding",]
+
+#p6 Action
+
+p6 = ["Hardcore", "Softcore", "3some", "Orgy", "Group sex", "Underwater sex", "Brutal", "kinky", "Gangbang", "BDSM", "Masturbation",]
+
+
+
+
+a = random.choice(p1)
+b = random.choice(p2)
+c = random.choice(p3)
+d = random.choice(p4)
+e = random.choice(p5)
+f = random.choice(p6)
+print("Resultant Query:")
+print("|--------------|")
+print("")
+print("Category:")
+print("")
+print ("    " + a)
+print("         ")
+print ("Country:")
+print ("")
+print ("    " + b)
+print ("")
+print ("Characteristics:")
+print ("")
+print ("    " + c)
+print("")
+print("Profession:")
+print ("")
+print ("    " + d)
+print ("")
+print ("Fetish:")
+print ("")
+print ("    " + e)
+print ("")
+print("Action:")
+print ("")
+print ("    "+ f)
+print ("")
+print ("|------------|")
+print("       ")
+print("For documentation and support visit http://bit.ly/2honreb ")
+print("Ver 2.0")
